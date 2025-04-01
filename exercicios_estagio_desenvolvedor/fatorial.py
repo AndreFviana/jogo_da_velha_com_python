a637939b6b4d33239c4a3c08d8b0ab55112c4b99
Andre Viana <91017517+AndreFviana@users.noreply.github.com>
2025-03-31 22:53:21 -0300

    funções para calculo de fatorial

---
+++ exercicios_estagio_desenvolvedor/fatorial.py
@@ -0,0 +1,47 @@
+import math
+
+#calculando fatorial
+def fatorial(n):
+    resultado= 1
+    for i in range(n,0,-1):
+        resultado *= i
+    return resultado
+num = int(input("digite um numero: "))
+print(f"o fatorial de {num} é {fatorial(num)}")
+"""
+#versão sem o return
+def fatorial(n):
+    resultado= 1
+    for i in range(n,0,-1):
+        resultado *= i
+    print(f"o fatorial de {n} é {resultado}")
+
+print("ola")    
+fatorial(3)
+"""
+"""
+#fatorial com while
+def fatorial(n):
+    resultado= 1
+    while n>1:
+        resultado *= n
+        n -= 1
+    return resultado
+n=3
+resultado =fatorial(n)
+print(f"fatorial de:{n} é {fatorial(n)} ")"
+"""
+
+#fatorial com listas
+def lista_fatorial(lista):
+    fatorias = []
+    for n in lista:
+        fatorias.append(math.factorial(nms))
+    return fatorias    
+numeros = [3,4,5]
+cpf_input = input("digite seu cpf")
+#convertendo str para int
+nms = list(map(int,cpf_input.split()))
+print(nms)
+resultado = lista_fatorial(nms)
+print(f"o fatorial de {', '.join(map(str, nms))} é {', '.join(map(str,resultado))}")
